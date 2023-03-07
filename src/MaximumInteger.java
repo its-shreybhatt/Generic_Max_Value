2d76c5386ae3be81aac0c3938cdcc22be5ccf50d
its-shreybhatt <shreybhatt2@gmail.com>
2023-03-07 15:52:15 +0530

    Test Case 1: Maximum String value at 1st Position

--- src/MaximumInteger.java
+++ src/MaximumInteger.java
@@ -37,7 +37,7 @@ public class MaximumInteger {
     public static void main(String[] args) {
         Integer xInt = 9, yInt = 14, zInt = 25;
         Float xF1 = 8.6f, yF1 = 10.8f, zF1 = 19.7f;
-        String xStr="pear" , yStr= "apple" , zStr = "orange";
+        String xStr="van" , yStr= "apple" , zStr = "orange";
 
         MaximumInteger.testMax(xInt, yInt, zInt);
         MaximumInteger.testMax(xF1, yF1, zF1);
