b0b7fa159c33760d816a87b81c74c03005c97dbb
its-shreybhatt <shreybhatt2@gmail.com>
2023-03-07 15:18:56 +0530

    Added : Code to find Maximum Float Using Generics

--- src/MaximumInteger.java
+++ src/MaximumInteger.java
@@ -9,12 +9,25 @@ public class MaximumInteger {
             max = cInt;
         }
         System.out.println("Maximum Integer = " + max);
+    }
+
+    public static void testMax(Float aF1, Float bF1, Float cF1) {
+        Float max = aF1;
+        if (bF1.compareTo(max) > 0) {
+            max = bF1;
+        }
+        if (cF1.compareTo(max) > 0) {
+            max = cF1;
+        }
+        System.out.println("Maximum Float = " + max);
 
     }
 
     public static void main(String[] args) {
         Integer xInt = 9, yInt = 14, zInt = 25;
+        Float xF1 = 8.6f, yF1 = 3.8f, zF1 = 7.7f;
 
         MaximumInteger.testMax(xInt, yInt, zInt);
+        MaximumInteger.testMax(xF1, yF1, zF1);
     }
 }
