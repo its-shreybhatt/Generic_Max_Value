f50ace5ad1b154e11547cfe1eabd4d24d4bc30ee
its-shreybhatt <shreybhatt2@gmail.com>
2023-03-07 15:06:35 +0530

    Test Case 1: Maximum Integer at 1st Position

--- src/MaximumInteger.java
+++ src/MaximumInteger.java
@@ -1,18 +1,20 @@
 public class MaximumInteger {
 
-    public static void testMax (Integer aInt, Integer bInt, Integer cInt){
+    public static void testMax(Integer aInt, Integer bInt, Integer cInt) {
         Integer max = aInt;
-        if (bInt.compareTo(max)>0){
+        if (bInt.compareTo(max) > 0) {
             max = bInt;
         }
-        if (cInt.compareTo(max)>0){
+        if (cInt.compareTo(max) > 0) {
             max = cInt;
         }
-        System.out.println("Maximum Integer = " +max);
-    }
-    public static void main(String[] args) {
-        Integer xInt = 9, yInt =4, zInt = 5;
+        System.out.println("Maximum Integer = " + max);
 
-        MaximumInteger.testMax(xInt,yInt,zInt);
+    }
+
+    public static void main(String[] args) {
+        Integer xInt = 9, yInt = 4, zInt = 5;
+
+        MaximumInteger.testMax(xInt, yInt, zInt);
     }
 }
